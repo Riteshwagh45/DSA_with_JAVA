8a05cebf1eaee00eabc311443430af326354119d
Ritesh Wagh <waghritesh777@gmail.com>
2026-01-09 00:08:01 +0530

    Create NQueens_2.java

---
+++ DAY_19/NQueens_2.java
@@ -0,0 +1,51 @@
+class NQueens_2 {
+
+    int count = 0;
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        board1(board, 0);
+        return count;
+    }
+
+    public boolean issafe2(char[][] board, int row, int col) {
+        // vertically up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') return false;
+        }
+
+        // diagonal left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        // diagonal right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+
+    public void board1(char[][] board, int row) {
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            if (issafe2(board, row, j)) {
+                board[row][j] = 'Q';
+                board1(board, row + 1);
+                board[row][j] = 'X';
+            }
+        }
+    }
+}
