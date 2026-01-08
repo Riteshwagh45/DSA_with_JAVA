9481cc8ef5225032bbe427abb39f8d1263a691dd
Ritesh Wagh <waghritesh777@gmail.com>
2026-01-09 00:13:38 +0530

    Update NQueens.java

--- DAY_19/NQueens.java
+++ DAY_19/NQueens.java
@@ -1,73 +1,64 @@
-public class NQueens {
+/*
+ This code runs on LeetCode only.
+ There is no main() method here because LeetCode
+ already has its own main class.
 
-    // Check if it's safe to place a queen at board[row][col]
-    public static boolean isSafe(char board[][], int row, int col) {
+ To run this code in your editor,
+ create a main class and call this function.
+*/
 
-        // Vertical up
-        for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
-        }
+import java.util.*;
 
-        // Diagonal left up
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
+class Solution {
 
-        // Diagonal right up
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
+    List<List<String>> result = new ArrayList<>();
 
-        return true;
-    }
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
 
-    // Solve the N-Queens problem recursively
-    public static void nQueens(char board[][], int row) {
-        // Base case: All rows filled
-        if (row == board.length) {
-            printBoard(board);
-            return;
-        }
-
-        // Try placing a queen in each column
-        for (int j = 0; j < board.length; j++) {
-            if (isSafe(board, row, j)) {
-                board[row][j] = 'Q'; // Place queen
-                nQueens(board, row + 1); // Recur
-                board[row][j] = 'x'; // Backtrack
-            }
-        }
-    }
-
-    // Print the chess board
-    public static void printBoard(char board[][]) {
-        System.out.println("_________ Chess Board _________");
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    // Main method
-    public static void main(String args[]) {
-        int n = 4;
-        char board[][] = new char[n][n];
-
-        // Initialize the board with 'x'
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = 'x';
+                board[i][j] = '.';
             }
         }
 
         nQueens(board, 0);
+        return result;
+    }
+
+    public void nQueens(char[][] board, int row) {
+        if (row == board.length) {
+            List<String> temp = new ArrayList<>();
+            for (int i = 0; i < board.length; i++) {
+                temp.add(new String(board[i]));
+            }
+            result.add(temp);
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1);
+                board[row][j] = '.';
+            }
+        }
+    }
+
+    public boolean isSafe(char[][] board, int row, int col) {
+
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') return false;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        return true;
     }
 }
